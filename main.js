4d8928fd455335881375d309f56c3cbad916450d
principal Kelvo <57545486+principalkelvo@users.noreply.github.com>
2019-11-09 17:58:07 +0300

    First commit

---
+++ main.js
@@ -0,0 +1,12 @@
+let bmw = document.getElementById("bmw");
+let re = document.getElementById("re");
+
+function main() {
+    bmw.addEventListener('click', () => game('bmw'));
+    re.addEventListener('click', () => game('re'));
+};
+
+function game() {
+    console.log("love")
+}
+main();
