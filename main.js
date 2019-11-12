7074712f57303eefbf09b966b892b253d90d6f92
VictorOmondi1997 <info.victoromondi@gmail.com>
2019-11-12 23:09:08 +0300

    Update main.js
    Add myName() function to return the name of contributors

--- main.js
+++ main.js
@@ -9,4 +9,7 @@ function main() {
 function game() {
     console.log("love")
 }
+function myName(){
+    window.alert("Your name is Kevin Njoro Owino \nThis Code has been edited with Victor Omondi")
+}
 main();
